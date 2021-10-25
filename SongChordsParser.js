41cc6c336c88b46ca16ab688f08e32927506db6c
Shushik <silkleopard@yandex.ru>
2021-10-26 01:34:59 +0300

    Update SongChordsParser.js

--- SongChordsParser.js
+++ SongChordsParser.js
@@ -726,7 +726,6 @@ export default class Self {
         let types = VERSE_TYPES.join('|');
 
         raw = raw.
-              replace(new RegExp(`(\\[\\/(${types})\\])\n{1}`), '$1\n\n').
               replace(/\t/g, ' ').
               replace(/\r/, '').
               replace(/ {3,}/g, '  ').
